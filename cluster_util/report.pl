acfc93a544c86cd32de061bb69baedcf880fbad5
Chris Scheller <schelcj@umich.edu>
2011-10-19 12:02:30 -0400

    * zeroed in on the chart output with the gplot.pl command args   * added a new target to create a pdf   * dropped the map() in a void context for a easier to understand foreach   * get data for the previous 12 months   * change the xlabels a bit to work well in the output

--- cluster_util/report.pl
+++ cluster_util/report.pl
@@ -1,35 +1,38 @@
 #!/usr/bin/perl
 
+## no critic (ValuesAndExpressions::ProhibitMagicNumbers, ValuesAndExpressions::ProhibitMismatchedOperators)
+
 use Modern::Perl;
 use Class::Date;
 use System::Command;
 use File::Slurp qw(append_file);
 
 my $now         = Class::Date->now();
-my $date        = Class::Date->new([$now->year, qw(01 01 00 00 00)]);
 my $report_cmd  = q{sreport -P -n cluster util start=%s end=%s};
 my @headers     = (qw(cluster allocated down plnd_down idle reserved reported));
 my %files       = map {$_ => qq{${_}.data}} @headers;
 my $util_ref    = {};
-my $yaxis_scale = 1000000;
+my $yaxis_scale = 1_000_000;
 
-for (1 .. $now->month) {
-  my $start       = $date->month_begin->strftime('%Y-%m-%d');
-  my $end         = $date->month_end->strftime('%Y-%m-%d');
+for (1 .. 12) {
+  my $start       = $now->month_begin->strftime('%Y-%m-%d');
+  my $end         = $now->month_end->strftime('%Y-%m-%d');
   my $command     = sprintf $report_cmd, $start, $end;
   my $results_ref = get_util($command);
 
   $util_ref->{$start} = $results_ref;
 
-  $date += '1M';    ## no critic (ValuesAndExpressions::ProhibitMismatchedOperators)
+  $now -= '1M';
 }
 
 foreach my $date (sort keys %{$util_ref}) {
-  (my $xaxis_label = $date) =~ s/\d{4}\-(\d{2})\-\d{2}/$1/g;
-  map {
-    my $data_point = $util_ref->{$date}->{$_} / $yaxis_scale;
-    append_file($files{$_}, qq#$xaxis_label $data_point\n#)
-  } keys %{$util_ref->{$date}};
+  (my $xaxis_label = $date) =~ s/\d{2}(\d{2})\-(\d{2})\-\d{2}/$1\/$2/g;
+
+  foreach my $dpoint (keys %{$util_ref->{$date}}) {
+    my $data_point = $util_ref->{$date}->{$dpoint} / $yaxis_scale;
+
+    append_file($files{$dpoint}, qq#$xaxis_label $data_point\n#)
+  }
 }
 
 sub get_util {
