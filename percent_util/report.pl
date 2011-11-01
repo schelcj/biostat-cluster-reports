376f88ea0d9bfa810e35afdca517821e9754c57a
Chris Scheller <schelcj@umich.edu>
2011-11-01 11:22:47 -0400

    switched to a excel report for easier formatting

--- percent_util/report.pl
+++ percent_util/report.pl
@@ -5,19 +5,23 @@
 use Modern::Perl;
 use Class::Date;
 use System::Command;
-use File::Slurp qw(append_file);
+use Excel::Template;
 
+my $template    = 'template.xml';
 my $report_cmd  = q{sreport -P -n cluster util start=%s end=%s};
 my @headers     = (qw(cluster allocated down plnd_down idle reserved reported));
 my $yaxis_scale = 1_000_000;
 my $date_format = q{%Y-%m-%d};
+my $report      = 'report.xls';
 
 my $now        = Class::Date->now();
+my $excel      = Excel::Template->new(filename => $template);
 my $start_date = $now - '12M';
 my $start      = $start_date->month_begin->strftime($date_format);
 my $end        = $now->month_end->strftime($date_format);
 my $command    = sprintf $report_cmd, $start, $end;
 
+my $params = {};
 my %item   = ();
 my $cmd    = System::Command->new($command);
 my $stdout = $cmd->stdout();
@@ -29,9 +33,14 @@ while (<$stdout>) {
 
 $cmd->close();
 
-say "Total Cluster Utilization for ($start - $end)";
-say '-------------------------------------------------------';
+$params->{caption} = "Total Cluster Utilization for ($start - $end)";
+
 foreach my $header (@headers) {
   next if $header eq 'cluster';
-  printf "%-45s %.2f%%\n", ucfirst($header), (($item{$header} / $item{reported}) * 100);
+  my $percent = sprintf '%.2f', (($item{$header} / $item{reported}) * 100);
+  push @{$params->{results}}, {header => ucfirst($header), percent => $percent};
 }
+
+$excel->param($params);
+$excel->write_file($report);
+
