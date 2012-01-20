e918fa196a628d3985dc69de7a097fd7c67dd945
Chris Scheller <schelcj@umich.edu>
2012-01-20 15:51:35 -0500

    pulling all data points and cleaned up for perlcritic

--- wait_idle_time/report.pl
+++ wait_idle_time/report.pl
@@ -4,47 +4,34 @@ use Modern::Perl;
 use Class::Date;
 use System::Command;
 use Readonly;
-use Data::Dumper;
 
-Readonly::Array my @SACCT_HEADERS   => (qw(jobid user submit start end));
-Readonly::Array my @SREPORT_HEADERS => (qw(clst alloc down plnd_down idle resv rep));
+Readonly::Array my @SACCT_HEADERS    => (qw(jobid user submit start end));
+Readonly::Array my @SREPORT_HEADERS  => (qw(clst alloc down plnd_down idle resv rep));
+Readonly::Array my @SACCTMGR_HEADERS => (qw(name account admin));
 
 Readonly::Scalar my $COMMA       => q{,};
 Readonly::Scalar my $PIPE        => q{|};
 Readonly::Scalar my $DATE_FORMAT => q{%Y-%m-%d};
 
-Readonly::Scalar my $SACCT_CMD   => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s}, join($COMMA, @SACCT_HEADERS);
-Readonly::Scalar my $SREPORT_CMD => q{sreport -n -P cluster util start=%s};
+Readonly::Scalar my $SACCT_CMD    => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s}, join($COMMA, @SACCT_HEADERS);
+Readonly::Scalar my $SREPORT_CMD  => q{sreport -n -P cluster util start=%s};
+Readonly::Scalar my $SACCTMGR_CMD => q{sacctmgr -n -P show users};
 
-my $now          = Class::Date->now();
-my $start_date   = $now - '1M';
-my @wait_results = get_results($start_date, $SACCT_CMD, @SACCT_HEADERS);
-my $avg_wait     = get_avg_wait_time(@wait_results);
+my $now        = Class::Date->now();
+my $start_date = $now - '1M'; ## no critic (ProhibitMismatchedOperators)
+my $avg_wait   = get_avg_wait_time($start_date);
+my $idle_time  = get_total_idle_time($start_date);
+my $total_users = get_total_users($start_date);
 
-say 'Total jobs completed in the last month ' . scalar @wait_results;
 say "Average waittime for all jobs for the last month is $avg_wait minutes";
-
-sub get_results {
-  my ($date, $cmd_fmt, @headers) = @_;
-
-  my @results = ();
-  my $command = sprintf $cmd_fmt, $date->strftime($DATE_FORMAT);
-  my $cmd     = System::Command->new($command);
-  my $stdout  = $cmd->stdout();
-
-  while (<$stdout>) {
-    chomp;
-    my %record = ();
-    @record{@headers} = split(/[$PIPE]/);
-    push @results, \%record;
-  }
-
-  $cmd->close();
-  return @results;
-}
+say "Total CPU Minutes idle: $idle_time";
+say "Total users: $total_users";
 
 sub get_avg_wait_time {
-  my (@results)  = @_;
+  my ($date) = @_;
+  my $command = sprintf $SACCT_CMD, $date->strftime($DATE_FORMAT);
+  my @results = _get_results($command, @SACCT_HEADERS);
+
   my $total_wait = 0;
 
   foreach my $result (@results) {
@@ -56,4 +43,35 @@ sub get_avg_wait_time {
   }
 
   return sprintf '%.0f', $total_wait / scalar @results;
+}
+
+sub get_total_idle_time {
+  my ($date) = @_;
+  my $command = sprintf $SREPORT_CMD, $date->strftime($DATE_FORMAT);
+  my @results = _get_results($command, @SREPORT_HEADERS);
+  return $results[0]->{idle};
+}
+
+sub get_total_users {
+  my ($date)  = @_;
+  my @results = _get_results($SACCTMGR_CMD, @SACCTMGR_HEADERS);
+  return scalar @results;
+}
+
+sub _get_results {
+  my ($command, @headers) = @_;
+
+  my @results = ();
+  my $cmd     = System::Command->new($command);
+  my $stdout  = $cmd->stdout();
+
+  while (<$stdout>) {
+    chomp;
+    my %result = ();
+    @result{@headers} = split(/[$PIPE]/);
+    push @results, \%result;
+  }
+
+  $cmd->close();
+  return @results;
 }
