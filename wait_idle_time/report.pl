2e2a22c303593f3facdcf4e23313fbccbfbdf517
Chris Scheller <schelcj@umich.edu>
2012-01-20 15:30:47 -0500

    refactored to start pulling the other data points

--- wait_idle_time/report.pl
+++ wait_idle_time/report.pl
@@ -6,39 +6,36 @@ use System::Command;
 use Readonly;
 use Data::Dumper;
 
+Readonly::Array my @SACCT_HEADERS   => (qw(jobid user submit start end));
+Readonly::Array my @SREPORT_HEADERS => (qw(clst alloc down plnd_down idle resv rep));
+
 Readonly::Scalar my $COMMA       => q{,};
 Readonly::Scalar my $PIPE        => q{|};
 Readonly::Scalar my $DATE_FORMAT => q{%Y-%m-%d};
-Readonly::Scalar my $SACCT_CMD   => q{sacct -a -n -X -s cd -P -S %s -o %s};
 
-Readonly::Array my @HEADERS      => (qw(jobid user submit start end));
+Readonly::Scalar my $SACCT_CMD   => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s}, join($COMMA, @SACCT_HEADERS);
+Readonly::Scalar my $SREPORT_CMD => q{sreport -n -P cluster util start=%s};
 
-my $now        = Class::Date->now();
-my $start_date = $now - '1M';
-my $command    = get_command($start_date);
-my @results    = get_results($command);
-my $total_wait = get_total(@results);
-my $total_jobs = scalar @results;
-my $avg_wait   = sprintf '%.0f', $total_wait / $total_jobs;
+my $now          = Class::Date->now();
+my $start_date   = $now - '1M';
+my @wait_results = get_results($start_date, $SACCT_CMD, @SACCT_HEADERS);
+my $avg_wait     = get_avg_wait_time(@wait_results);
 
-say "Total jobs completed in the last month $total_jobs"; 
+say 'Total jobs completed in the last month ' . scalar @wait_results;
 say "Average waittime for all jobs for the last month is $avg_wait minutes";
 
-sub get_command {
-  my ($date) = @_;
-  return sprintf $SACCT_CMD, $date->strftime($DATE_FORMAT), join($COMMA, @HEADERS);
-}
-
 sub get_results {
-  my ($command) = @_;
-  my @results   = ();
-  my $cmd       = System::Command->new($command);
-  my $stdout    = $cmd->stdout();
+  my ($date, $cmd_fmt, @headers) = @_;
+
+  my @results = ();
+  my $command = sprintf $cmd_fmt, $date->strftime($DATE_FORMAT);
+  my $cmd     = System::Command->new($command);
+  my $stdout  = $cmd->stdout();
 
   while (<$stdout>) {
     chomp;
     my %record = ();
-    @record{@HEADERS} = split(/[$PIPE]/);
+    @record{@headers} = split(/[$PIPE]/);
     push @results, \%record;
   }
 
@@ -46,17 +43,17 @@ sub get_results {
   return @results;
 }
 
-sub get_total {
-  my (@results) = @_;
-  my $total     = 0;
+sub get_avg_wait_time {
+  my (@results)  = @_;
+  my $total_wait = 0;
 
   foreach my $result (@results) {
     my $submit = Class::Date->new($result->{submit});
     my $start  = Class::Date->new($result->{start});
     my $diff   = $start - $submit;
 
-    $total += $diff->min;
+    $total_wait += $diff->min;
   }
 
-  return $total;
+  return sprintf '%.0f', $total_wait / scalar @results;
 }
