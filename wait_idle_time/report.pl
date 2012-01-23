e7bc5f16c215369aa245e30240bac0263e299d10
Chris Scheller <schelcj@umich.edu>
2012-01-23 10:21:49 -0500

    spit out the report in excel. may need to go back a few months for comparisons

--- wait_idle_time/report.pl
+++ wait_idle_time/report.pl
@@ -4,6 +4,7 @@ use Modern::Perl;
 use Class::Date;
 use System::Command;
 use Readonly;
+use Excel::Template;
 
 Readonly::Array my @SACCT_HEADERS    => (qw(jobid user submit start end));
 Readonly::Array my @SREPORT_HEADERS  => (qw(clst alloc down plnd_down idle resv rep));
@@ -12,20 +13,29 @@ Readonly::Array my @SACCTMGR_HEADERS => (qw(name account admin));
 Readonly::Scalar my $COMMA       => q{,};
 Readonly::Scalar my $PIPE        => q{|};
 Readonly::Scalar my $DATE_FORMAT => q{%Y-%m-%d};
+Readonly::Scalar my $TEMPLATE    => q{template.xml};
+Readonly::Scalar my $REPORT      => q{wait_idle_time.xls};
 
 Readonly::Scalar my $SACCT_CMD    => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s}, join($COMMA, @SACCT_HEADERS);
 Readonly::Scalar my $SREPORT_CMD  => q{sreport -n -P cluster util start=%s};
 Readonly::Scalar my $SACCTMGR_CMD => q{sacctmgr -n -P show users};
 
-my $now        = Class::Date->now();
-my $start_date = $now - '1M'; ## no critic (ProhibitMismatchedOperators)
-my $avg_wait   = get_avg_wait_time($start_date);
-my $idle_time  = get_total_idle_time($start_date);
+my $now         = Class::Date->now();
+my $excel       = Excel::Template->new(filename => $TEMPLATE);
+my $start_date  = $now - '1M'; ## no critic (ProhibitMismatchedOperators)
+my $avg_wait    = get_avg_wait_time($start_date);
+my $idle_ref    = get_total_idle_time($start_date);
 my $total_users = get_total_users($start_date);
 
-say "Average waittime for all jobs for the last month is $avg_wait minutes";
-say "Total CPU Minutes idle: $idle_time";
-say "Total users: $total_users";
+$excel->param({
+  total_users => $total_users,
+  avg_wait    => $avg_wait,
+  idle_time   => $idle_ref->{time},
+  idle_perc   => $idle_ref->{percent},
+  start_date  => $start_date->strftime($DATE_FORMAT),
+  end_date    => $now->strftime($DATE_FORMAT),
+});
+$excel->write_file($REPORT);
 
 sub get_avg_wait_time {
   my ($date) = @_;
@@ -47,9 +57,12 @@ sub get_avg_wait_time {
 
 sub get_total_idle_time {
   my ($date) = @_;
-  my $command = sprintf $SREPORT_CMD, $date->strftime($DATE_FORMAT);
-  my @results = _get_results($command, @SREPORT_HEADERS);
-  return $results[0]->{idle};
+
+  my $command   = sprintf $SREPORT_CMD, $date->strftime($DATE_FORMAT);
+  my ($results) = _get_results($command, @SREPORT_HEADERS);
+  my $percent   = sprintf '%.2f', (($results->{idle} / $results->{rep}) * 100); ## no critic (ProhibitMagicNumbers)
+
+  return {'time' => $results->{idle}, 'percent' => $percent};
 }
 
 sub get_total_users {
