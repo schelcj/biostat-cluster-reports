7016e8c3bce4bea187c0efc79feb25a69a78780e
Chris Scheller <schelcj@umich.edu>
2012-02-01 14:43:14 -0500

    report on a monthly basis over the last year

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: cluster_util_report top_usage_report percent_util_report compile
+all: cluster_util_report top_usage_report percent_util_report wait_idle_time_report compile
 
 cluster_util_report: 
 	$(MAKE) -C cluster_util/
@@ -9,8 +9,12 @@ top_usage_report:
 percent_util_report:
 	$(MAKE) -C percent_util/
 
+wait_idle_time_report:
+	$(MAKE) -C wait_idle_time/
+
 compile:
 	cp cluster_util/chart.pdf reports/
 	cp top_usage/top_usage.xls reports/
 	cp percent_util/percent_util.xls reports/
 	cp percent_util/percent_util_by_month.xls reports/
+	cp wait_idle_time/wait_idle_time.xls reports/
--- wait_idle_time/report.pl
+++ wait_idle_time/report.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+## no critic (ProhibitMismatchedOperators)
+
 use Modern::Perl;
 use Class::Date;
 use System::Command;
@@ -15,39 +17,50 @@ Readonly::Scalar my $PIPE        => q{|};
 Readonly::Scalar my $DATE_FORMAT => q{%Y-%m-%d};
 Readonly::Scalar my $TEMPLATE    => q{template.xml};
 Readonly::Scalar my $REPORT      => q{wait_idle_time.xls};
+Readonly::Scalar my $MAX_MONTHS  => 12;
 
-Readonly::Scalar my $SACCT_CMD    => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s}, join($COMMA, @SACCT_HEADERS);
-Readonly::Scalar my $SREPORT_CMD  => q{sreport -n -P cluster util start=%s};
+Readonly::Scalar my $SACCT_CMD    => sprintf q{sacct -a -n -X -s cd -P -o %s -S %%s -E %%s}, join($COMMA, @SACCT_HEADERS);
+Readonly::Scalar my $SREPORT_CMD  => q{sreport -n -P cluster util start=%s end=%s};
 Readonly::Scalar my $SACCTMGR_CMD => q{sacctmgr -n -P show users};
 
-my $now         = Class::Date->now();
 my $excel       = Excel::Template->new(filename => $TEMPLATE);
-my $start_date  = $now - '1M'; ## no critic (ProhibitMismatchedOperators)
-my $avg_wait    = get_avg_wait_time($start_date);
-my $idle_ref    = get_total_idle_time($start_date);
-my $total_users = get_total_users($start_date);
+my $now         = Class::Date->now();
+my $start_date  = $now - '1M';
+my $end_date    = $now;
+my $total_users = get_total_users();
+my $params      = {};
 
-$excel->param({
-  total_users => $total_users,
-  avg_wait    => $avg_wait,
-  idle_time   => $idle_ref->{time},
-  idle_perc   => $idle_ref->{percent},
-  start_date  => $start_date->strftime($DATE_FORMAT),
-  end_date    => $now->strftime($DATE_FORMAT),
-});
+for (1..$MAX_MONTHS) {
+  my $avg_wait = get_avg_wait_time($start_date,$end_date);
+  my $idle_ref = get_total_idle_time($start_date,$end_date);
+
+  push @{$params->{results}}, {
+    total_users => $total_users,
+    avg_wait    => $avg_wait,
+    idle_time   => $idle_ref->{time},
+    idle_perc   => $idle_ref->{percent},
+    start_date  => $start_date->strftime($DATE_FORMAT),
+    end_date    => $end_date->strftime($DATE_FORMAT),
+  };
+
+  $start_date = $start_date - '1M';
+  $end_date   = $end_date   - '1M';
+}
+
+$excel->param($params);
 $excel->write_file($REPORT);
 
 sub get_avg_wait_time {
-  my ($date) = @_;
-  my $command = sprintf $SACCT_CMD, $date->strftime($DATE_FORMAT);
-  my @results = _get_results($command, @SACCT_HEADERS);
+  my ($start,$end) = @_;
 
+  my $command    = sprintf $SACCT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
+  my @results    = _get_results($command, @SACCT_HEADERS);
   my $total_wait = 0;
 
   foreach my $result (@results) {
-    my $submit = Class::Date->new($result->{submit});
-    my $start  = Class::Date->new($result->{start});
-    my $diff   = $start - $submit;
+    my $job_submit = Class::Date->new($result->{submit});
+    my $job_start  = Class::Date->new($result->{start});
+    my $diff       = $job_start - $job_submit;
 
     $total_wait += $diff->min;
   }
@@ -56,9 +69,9 @@ sub get_avg_wait_time {
 }
 
 sub get_total_idle_time {
-  my ($date) = @_;
+  my ($start,$end) = @_;
 
-  my $command   = sprintf $SREPORT_CMD, $date->strftime($DATE_FORMAT);
+  my $command   = sprintf $SREPORT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
   my ($results) = _get_results($command, @SREPORT_HEADERS);
   my $percent   = sprintf '%.2f', (($results->{idle} / $results->{rep}) * 100); ## no critic (ProhibitMagicNumbers)
 
@@ -66,7 +79,6 @@ sub get_total_idle_time {
 }
 
 sub get_total_users {
-  my ($date)  = @_;
   my @results = _get_results($SACCTMGR_CMD, @SACCTMGR_HEADERS);
   return scalar @results;
 }
