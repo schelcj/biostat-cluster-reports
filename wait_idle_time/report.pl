6baadbee879dfbddb996aacd0cb105608b1a3385
Chris Scheller <schelcj@umich.edu>
2012-06-25 15:42:36 -0400

    Ugh, had some old changes laying around   * added total users   * average job duration   * some perltidy cleanup   * recording a data file for graphing in

--- wait_idle_time/report.pl
+++ wait_idle_time/report.pl
@@ -7,6 +7,7 @@ use Class::Date;
 use System::Command;
 use Readonly;
 use Excel::Template;
+use File::Slurp qw(write_file append_file);
 
 Readonly::Array my @SACCT_HEADERS    => (qw(jobid user submit start end));
 Readonly::Array my @SREPORT_HEADERS  => (qw(clst alloc down plnd_down idle resv rep));
@@ -28,35 +29,43 @@ my $now         = Class::Date->now();
 my $start_date  = $now - '7D';
 my $end_date    = $now;
 my $total_users = get_total_users();
-my $params      = {};
 my @results     = ();
+my $params      = {
+  total_users   => get_total_users(),
+};
 
-for (1..$MAX_WEEKS) {
-  my $avg_wait = get_avg_wait_time($start_date,$end_date);
-  my $idle_ref = get_total_idle_time($start_date,$end_date);
+for (1 .. $MAX_WEEKS) {
+  my $avg_wait         = get_avg_wait_time($start_date, $end_date);
+  my $avg_job_duration = get_avg_job_duration($start_date, $end_date);
+  my $idle_ref         = get_total_idle_time($start_date, $end_date);
+  my $total_jobs       = get_total_jobs($start_date, $end_date);
 
   push @results, {
-    total_users => $total_users,
-    avg_wait    => $avg_wait,
-    idle_time   => $idle_ref->{time},
-    idle_perc   => $idle_ref->{percent},
-    start_date  => $start_date->strftime($DATE_FORMAT),
-  };
+         avg_wait         => $avg_wait,
+         avg_job_duration => $avg_job_duration,
+         total_jobs       => $total_jobs,
+         idle_time        => $idle_ref->{time},
+         idle_perc        => $idle_ref->{percent},
+         start_date       => $start_date->strftime($DATE_FORMAT),
+       };
 
   $start_date = $start_date - '7D';
-  $end_date   = $end_date   - '7D';
+  $end_date   = $end_date - '7D';
 }
 
 @{$params->{results}} = sort {$a->{start_date} cmp $b->{start_date}} @results;
+
+write_file('waittime.dat', qq{Date\tSeconds\n});
+map { append_file('waittime.dat', qq($_->{start_date}\t$_->{avg_wait}\n)) } @{$params->{results}};
 
 $excel->param($params);
 $excel->write_file($REPORT);
 
 sub get_avg_wait_time {
-  my ($start,$end) = @_;
+  my ($start, $end) = @_;
 
-  my $command    = sprintf $SACCT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
-  my @results    = _get_results($command, @SACCT_HEADERS);
+  my $command = sprintf $SACCT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
+  my @results = _get_results($command, @SACCT_HEADERS);
   my $total_wait = 0;
 
   foreach my $result (@results) {
@@ -70,12 +79,39 @@ sub get_avg_wait_time {
   return sprintf '%.0f', $total_wait / scalar @results;
 }
 
-sub get_total_idle_time {
-  my ($start,$end) = @_;
+sub get_avg_job_duration {
+  my ($start, $end) = @_;
 
-  my $command   = sprintf $SREPORT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
+  my $command = sprintf $SACCT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
+  my @results = _get_results($command, @SACCT_HEADERS);
+  my $total_duration = 0;
+
+  foreach my $result (@results) {
+    my $job_start = Class::Date->new($result->{start});
+    my $job_end   = Class::Date->new($result->{end});
+    my $diff      = $job_end - $job_start;
+
+    $total_duration += $diff->min;
+  }
+
+  return sprintf '%.0f', $total_duration / scalar @results;
+}
+
+sub get_total_jobs {
+  my ($start, $end) = @_;
+
+  my $command = sprintf $SACCT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
+  my @results = _get_results($command, @SACCT_HEADERS);
+
+  return scalar @results;
+}
+
+sub get_total_idle_time {
+  my ($start, $end) = @_;
+
+  my $command = sprintf $SREPORT_CMD, $start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT);
   my ($results) = _get_results($command, @SREPORT_HEADERS);
-  my $percent   = sprintf '%.2f', (($results->{idle} / $results->{rep}) * 100); ## no critic (ProhibitMagicNumbers)
+  my $percent = sprintf '%.2f', (($results->{idle} / $results->{rep}) * 100);    ## no critic (ProhibitMagicNumbers)
 
   return {'time' => $results->{idle}, 'percent' => $percent};
 }
