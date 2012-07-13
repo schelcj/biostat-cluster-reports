48e9067ae0e1278f04d800d45bc2a6fbade9b16a
Chris Scheller <schelcj@umich.edu>
2012-07-13 11:26:19 -0400

    got the yaxis labels working

--- cluster_util/plot.R
+++ cluster_util/plot.R
@@ -13,10 +13,9 @@ lines(util_data$reported,  type="o", lwd=2, lty=6, pch=25, col=line_colors[6])
 
 axis(1, 1:12, lab=F)
 text(axTicks(1), par("usr")[3] - 2, pos=1, offset=1.0, srt=0, adj=1, labels=util_data$date[seq(1,12,2)], xpd=T, cex=0.8)
+axis(2, at=axTicks(2), labels=c("0","5","10","15","20"), cex=0.8, tick=TRUE, line=NA, pos=NA)
 
-axis(2, at=c(0,1,2,3,4), labels=c("0","5","10","15","20"), cex=0.8, tick=TRUE, line=NA, pos=NA)
-
-title(ylab="CPU Minutes")
+title(ylab="CPU Minutes 10^6")
 legend(8, u_range[2], c("Allocated", "Down", "Planned Down", "Idle", "Reserved", "Reported"), col=line_colors, lty=1:6, pch=20:25)
 box()
 
