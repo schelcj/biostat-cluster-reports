69e801d6170d3eb5e4bec9c15eb280abc729ffa0
Chris Scheller <schelcj@umich.edu>
2012-07-13 19:10:44 -0400

    that is what i want, thanks veronica

--- cluster_util/plot.R
+++ cluster_util/plot.R
@@ -15,7 +15,7 @@ axis(1, 1:12, lab=F)
 text(axTicks(1), par("usr")[3] - 2, pos=1, offset=1.0, srt=0, adj=1, labels=util_data$date[seq(1,12,2)], xpd=T, cex=0.8)
 axis(2, at=axTicks(2), labels=c("0","5","10","15","20"), cex=0.8, tick=TRUE, line=NA, pos=NA)
 
-title(ylab="CPU Minutes 10^6")
+title(ylab=expression(paste("CPU Minutes ",10^6,sep="")))
 legend(8, u_range[2], c("Allocated", "Down", "Planned Down", "Idle", "Reserved", "Reported"), col=line_colors, lty=1:6, pch=20:25)
 box()
 
