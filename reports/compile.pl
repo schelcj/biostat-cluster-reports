e9128566c4c2ea972573bb4a68b53753c5a8a358
Chris Scheller <schelcj@umich.edu>
2012-11-30 11:39:28 -0500

    finalize this table

--- reports/compile.pl
+++ reports/compile.pl
@@ -3,22 +3,24 @@
 use Modern::Perl;
 use HTML::Template;
 use Spreadsheet::ParseExcel;
+use File::Slurp qw(write_file);
 use Data::Dumper;
 
-my $pu_ref = get_percent_util_ref();
-my $tmpl   = HTML::Template->new(filename => 'report.tex.tmpl', global_vars => 1);
+my %pu        = get_percent_util();
+my $tmpl      = HTML::Template->new(filename => 'report.tex.tmpl', global_vars => 1);
+my $param_ref = {%pu};
 
-$tmpl->param($pu_ref);
-say $tmpl->output();
+$tmpl->param($param_ref);
 
-sub get_percent_util_ref {
-  my $parser           = Spreadsheet::ParseExcel->new();
-  my $percent_util     = $parser->parse('percent_util.xls');
-  my $percent_util_ref = {};
+write_file('report.tex', $tmpl->output());
+
+sub get_percent_util {
+  my $parser       = Spreadsheet::ParseExcel->new();
+  my $percent_util = $parser->parse('percent_util.xls');
+  my %results      = ();
 
   for my $ws ($percent_util->worksheets()) {
     my ($row_min, $row_max) = $ws->row_range();
-    my ($col_min, $col_max) = $ws->col_range();
 
     for my $row (2 .. $row_max) {
       my $col1 = $ws->get_cell($row,0)->value(); 
@@ -27,13 +29,13 @@ sub get_percent_util_ref {
       $col1 =~ s/[_]/ /g;
       $col2 =~ s/[%]//g;
 
-      push @{$percent_util_ref->{pu_results}}, {col1 => $col1, col2 => $col2};
+      push @{$results{pu_results}}, {col1 => $col1, col2 => $col2};
     }
 
-    $percent_util_ref->{pu_title}   = $ws->get_cell(0,0)->value();
-    $percent_util_ref->{pu_col1_hd} = $ws->get_cell(1,0)->value();
-    $percent_util_ref->{pu_col2_hd} = $ws->get_cell(1,1)->value();
+    $results{pu_title}   = $ws->get_cell(0,0)->value();
+    $results{pu_col1_hd} = $ws->get_cell(1,0)->value();
+    $results{pu_col2_hd} = $ws->get_cell(1,1)->value();
   }
 
-  return $percent_util_ref;
+  return %results;
 }
