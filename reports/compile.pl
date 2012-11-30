2000d510bee74e0b75bc3bf5e258d4f462aa86bf
Chris Scheller <schelcj@umich.edu>
2012-11-30 13:45:21 -0500

    another table added

--- reports/compile.pl
+++ reports/compile.pl
@@ -6,25 +6,26 @@ use Spreadsheet::ParseExcel;
 use File::Slurp qw(write_file);
 use Data::Dumper;
 
-my %pu        = get_percent_util();
 my $tmpl      = HTML::Template->new(filename => 'report.tex.tmpl', global_vars => 1);
-my $param_ref = {%pu};
+my %pu        = get_percent_util();
+my %pum       = get_percent_util_by_month();
+my $param_ref = {%pu, %pum};
 
 $tmpl->param($param_ref);
 
 write_file('report.tex', $tmpl->output());
 
 sub get_percent_util {
-  my $parser       = Spreadsheet::ParseExcel->new();
-  my $percent_util = $parser->parse('percent_util.xls');
-  my %results      = ();
+  my $parser   = Spreadsheet::ParseExcel->new();
+  my $workbook = $parser->parse('percent_util.xls');
+  my %results  = ();
 
-  for my $ws ($percent_util->worksheets()) {
-    my ($row_min, $row_max) = $ws->row_range();
+  for my $worksheet($workbook->worksheets()) {
+    my ($row_min, $row_max) = $worksheet->row_range();
 
     for my $row (2 .. $row_max) {
-      my $col1 = $ws->get_cell($row,0)->value(); 
-      my $col2 = $ws->get_cell($row,1)->value();
+      my $col1 = $worksheet->get_cell($row,0)->value(); 
+      my $col2 = $worksheet->get_cell($row,1)->value();
      
       $col1 =~ s/[_]/ /g;
       $col2 =~ s/[%]//g;
@@ -32,9 +33,39 @@ sub get_percent_util {
       push @{$results{pu_results}}, {col1 => $col1, col2 => $col2};
     }
 
-    $results{pu_title}   = $ws->get_cell(0,0)->value();
-    $results{pu_col1_hd} = $ws->get_cell(1,0)->value();
-    $results{pu_col2_hd} = $ws->get_cell(1,1)->value();
+    $results{pu_title}   = $worksheet->get_cell(0,0)->value();
+    $results{pu_col1_hd} = $worksheet->get_cell(1,0)->value();
+    $results{pu_col2_hd} = $worksheet->get_cell(1,1)->value();
+  }
+
+  return %results;
+}
+
+sub get_percent_util_by_month {
+  my $parser   = Spreadsheet::ParseExcel->new();
+  my $workbook = $parser->parse('percent_util_by_month.xls');
+  my %results  = ();
+
+  for my $worksheet ($workbook->worksheets()) {
+    my ($row_min, $row_max) = $worksheet->row_range();
+    my ($col_min, $col_max) = $worksheet->col_range();
+
+    for ($col_min .. $col_max) {
+      my $key = 'pum_col' . $_ . '_hd';
+      $results{$key} = $worksheet->get_cell(1,$_)->value();
+    }
+
+    for my $row (2 .. $row_max) {
+      my $col_ref = {};
+
+      for my $col ($col_min .. $col_max) {
+        $col_ref->{qq{col$col}} = $worksheet->get_cell($row,$col)->value();
+      }
+
+      push @{$results{pum_results}}, $col_ref;
+    }
+
+    $results{pum_title} = $worksheet->get_cell(0,0)->value();
   }
 
   return %results;
