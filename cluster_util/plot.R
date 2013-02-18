28e2188a062d6e87c3b95fa2ddebe530f0451ca3
Chris Scheller <schelcj@umich.edu>
2013-02-18 15:12:25 -0500

    labels were off by one

--- cluster_util/plot.R
+++ cluster_util/plot.R
@@ -12,8 +12,8 @@ lines(util_data$reserved,  type="o", lwd=2, lty=5, pch=24, col=line_colors[5])
 lines(util_data$reported,  type="o", lwd=2, lty=6, pch=25, col=line_colors[6])
 
 axis(1, 1:12, lab=F)
-text(axTicks(1), par("usr")[3] - 2, pos=1, offset=1.0, srt=0, adj=1, labels=util_data$date[seq(1,12,2)], xpd=T, cex=0.8)
-axis(2, at=axTicks(2), labels=c("0","5","10","15","20", "25", "30", "35"), cex=0.8, tick=TRUE, line=NA, pos=NA)
+text(axTicks(1), par("usr")[3] - 2, pos=1, offset=1.0, srt=0, adj=1, labels=util_data$date[seq(2,12,2)], xpd=T, cex=0.8)
+axis(2, at=axTicks(2), labels=seq(0, length(axTicks(2))^2, 5), cex=0.8, tick=TRUE, line=NA, pos=NA)
 
 title(ylab=expression(paste("CPU Minutes ",10^6,sep="")))
 legend(1, u_range[2], c("Allocated", "Down", "Planned Down", "Idle", "Reserved", "Reported"), col=line_colors, lty=1:6, pch=20:25)
