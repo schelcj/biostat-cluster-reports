d48e0efaa30f6c5f5bd529dd62cd0b1ff29c054a
Chris Scheller <schelcj@umich.edu>
2013-02-19 13:20:46 -0500

    checkpoint

--- total_jobs/report.pl
+++ total_jobs/report.pl
@@ -5,23 +5,31 @@ use IPC::System::Simple qw(capture);
 use Readonly;
 use Class::Date;
 use Class::CSV;
+use File::Slurp qw(write_file);
 
 Readonly::Scalar my $WEEK        => q{7D};
 Readonly::Scalar my $DATE_FORMAT => q{%Y-%m-%d};
 Readonly::Scalar my $CMD         => q{sacct -a -n -X -P -S %s -E %s};
-Readonly::Array  my @HEADERS     => (qw(date total_jobs avg_jobs));
+Readonly::Scalar my $FILE        => q{report.dat};
+Readonly::Array  my @HEADERS     => (qw(date total_jobs));
 
+my $csv   = Class::CSV->new(fields => \@HEADERS);
 my $now   = Class::Date->now();
 my $start = $now - $WEEK;
 my $end   = $now;
 
+$csv->add_line({map {$_ => $_} @HEADERS});
+
 for (1 .. 52) {
   my $total_jobs = get_total_jobs_for_week($start->strftime($DATE_FORMAT), $end->strftime($DATE_FORMAT));
-  say $total_jobs;
+
+  $csv->add_line({date => $start->strftime($DATE_FORMAT), total_jobs => $total_jobs});
 
   $start = $start - $WEEK;
   $end   = $end - $WEEK;
 }
+
+write_file($FILE, $csv->string());
 
 sub get_total_jobs_for_week {
   my ($start_date, $end_date) = @_;
