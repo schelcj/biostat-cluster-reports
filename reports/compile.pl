e6e69610b90730f25a7686a9003946b91d1cf242
Chris Scheller <schelcj@umich.edu>
2013-02-28 08:55:47 -0500

    pure tidy changes from perltidy

--- reports/compile.pl
+++ reports/compile.pl
@@ -24,22 +24,22 @@ sub get_percent_util {
   my $workbook = $parser->parse('percent_util.xls');
   my %results  = ();
 
-  for my $worksheet($workbook->worksheets()) {
+  for my $worksheet ($workbook->worksheets()) {
     my ($row_min, $row_max) = $worksheet->row_range();
 
     for my $row (2 .. $row_max) {
-      my $col1 = $worksheet->get_cell($row,0)->value(); 
-      my $col2 = $worksheet->get_cell($row,1)->value();
-     
+      my $col1 = $worksheet->get_cell($row, 0)->value();
+      my $col2 = $worksheet->get_cell($row, 1)->value();
+
       $col1 =~ s/[_]/ /g;
       $col2 =~ s/[%]//g;
 
       push @{$results{pu_results}}, {col1 => $col1, col2 => $col2};
     }
 
-    $results{pu_title}   = $worksheet->get_cell(0,0)->value();
-    $results{pu_col1_hd} = $worksheet->get_cell(1,0)->value();
-    $results{pu_col2_hd} = $worksheet->get_cell(1,1)->value();
+    $results{pu_title}   = $worksheet->get_cell(0, 0)->value();
+    $results{pu_col1_hd} = $worksheet->get_cell(1, 0)->value();
+    $results{pu_col2_hd} = $worksheet->get_cell(1, 1)->value();
   }
 
   return %results;
@@ -56,20 +56,20 @@ sub get_percent_util_by_month {
 
     for ($col_min .. $col_max) {
       my $key = 'pum_col' . $_ . '_hd';
-      $results{$key} = $worksheet->get_cell(1,$_)->value();
+      $results{$key} = $worksheet->get_cell(1, $_)->value();
     }
 
     for my $row (2 .. $row_max) {
       my $col_ref = {};
 
       for my $col ($col_min .. $col_max) {
-        $col_ref->{qq{col$col}} = $worksheet->get_cell($row,$col)->value();
+        $col_ref->{qq{col$col}} = $worksheet->get_cell($row, $col)->value();
       }
 
       push @{$results{pum_results}}, $col_ref;
     }
 
-    $results{pum_title} = $worksheet->get_cell(0,0)->value();
+    $results{pum_title} = $worksheet->get_cell(0, 0)->value();
   }
 
   return %results;
@@ -86,14 +86,14 @@ sub get_top_usage {
 
     for ($col_min .. $col_max) {
       my $key = 'tu_col' . $_ . '_hd';
-      $results{$key} = $worksheet->get_cell(1,$_)->value();
+      $results{$key} = $worksheet->get_cell(1, $_)->value();
     }
 
     for my $row (2 .. $row_max) {
       my $col_ref = {};
 
       for my $col ($col_min .. $col_max) {
-        $col_ref->{qq{col$col}} = $worksheet->get_cell($row,$col)->value();
+        $col_ref->{qq{col$col}} = $worksheet->get_cell($row, $col)->value();
       }
 
       push @{$results{tu_results}}, $col_ref;
@@ -101,16 +101,16 @@ sub get_top_usage {
 
 
 
-    $results{tu_title} = $worksheet->get_cell(0,0)->value();
+    $results{tu_title} = $worksheet->get_cell(0, 0)->value();
   }
 
   return %results;
 }
 
 sub get_wait_idle_time {
-  my $parser   = Spreadsheet::ParseExcel->new();
-  my $workbook = $parser->parse('wait_idle_time.xls');
-  my %results  = ();
+  my $parser      = Spreadsheet::ParseExcel->new();
+  my $workbook    = $parser->parse('wait_idle_time.xls');
+  my %results     = ();
   my @wit_results = ();
 
   for my $worksheet ($workbook->worksheets()) {
@@ -119,14 +119,14 @@ sub get_wait_idle_time {
 
     for ($col_min .. $col_max) {
       my $key = 'wit_col' . $_ . '_hd';
-      $results{$key} = $worksheet->get_cell(0,$_)->value();
+      $results{$key} = $worksheet->get_cell(0, $_)->value();
     }
 
     for my $row (1 .. 52) {
       my $col_ref = {};
 
       for my $col ($col_min .. $col_max) {
-        $col_ref->{qq{col$col}} = $worksheet->get_cell($row,$col)->value();
+        $col_ref->{qq{col$col}} = $worksheet->get_cell($row, $col)->value();
       }
 
       push @wit_results, $col_ref;
