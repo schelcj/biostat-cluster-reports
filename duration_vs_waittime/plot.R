2a044d40123fcfdb4119c371edf43c2ae7790301
Chris Scheller <schelcj@umich.edu>
2013-03-04 11:45:28 -0500

    generating plots by month for the last six months. the current month doesn't represent a good sample

--- duration_vs_waittime/plot.R
+++ duration_vs_waittime/plot.R
@@ -1,3 +1,8 @@
-report <- read.table('report.dat', header=T, sep=",")
-attach(report)
-plot(duration, waittime, xlab="Duration", ylab="Waittime", pch=19, cex=0.5, col="blue")
+build_reports <- function(file) {
+  report <- read.table(file, header=T, sep=",")
+  period <- sub('^report-', '', sub('.dat$', '', file))
+  pdf(paste('report-', period, '.pdf', sep=''))
+  plot(report$duration, report$waittime, xlab="Duration (Hours)", ylab="Waittime (Hours)", main=period, pch=19, cex=0.25, col="blue")
+  dev.off()
+}
+lapply(list.files(path='.', pattern='.dat$'), build_reports)
--- duration_vs_waittime/report.pl
+++ duration_vs_waittime/report.pl
@@ -6,42 +6,45 @@ use Class::Date;
 use Class::CSV;
 use File::Slurp qw(write_file);
 use Readonly;
-use Data::Dumper;
 
 Readonly::Scalar my $COMMA     => q{,};
 Readonly::Scalar my $PIPE      => qr{\|};
 Readonly::Scalar my $MONTH     => q{1M};
 Readonly::Scalar my $DATE_FMT  => q{%Y-%m-%d};
-Readonly::Scalar my $DATA_FILE => q{report.dat};
+Readonly::Scalar my $DATA_FILE => q{report-%s.dat};
 Readonly::Scalar my $CMD       => q{sacct -P -n -a -X -s cd -S %s -E %s -o %s};
+Readonly::Array my @HEADERS    => (qw(jobid submit start end));
 
-Readonly::Array my @HEADERS => (qw(jobid submit start end));
+my $now   = Class::Date->now();
+my $start = $now - $MONTH;
+my $end   = $now;
 
-my $csv     = Class::CSV->new(fields => [qw(jobid waittime duration)]);
-my $now     = Class::Date->now();
-my $start   = $now - $MONTH;
-my $end     = $now;
-my @results = ();
+for (1 .. 6) {
+  my $csv       = Class::CSV->new(fields => [qw(jobid waittime duration)]);
+  my $start_fmt = $start->strftime($DATE_FMT);
+  my $end_fmt   = $end->strftime($DATE_FMT);
+  my $command   = sprintf $CMD, $start_fmt, $end_fmt, join($COMMA, @HEADERS);
+  my $data_file = sprintf $DATA_FILE, $start_fmt;
 
-$csv->add_line({jobid => 'jobid', waittime => 'waittime', duration => 'duration'});
+  $csv->add_line({jobid => 'jobid', waittime => 'waittime', duration => 'duration'});
 
-my $start_fmt = $start->strftime($DATE_FMT);
-my $end_fmt   = $end->strftime($DATE_FMT);
-my $command   = sprintf $CMD, $start_fmt, $end_fmt, join($COMMA, @HEADERS);
+  for (capture($command)) {
+    chomp;
 
-for (capture($command)) {
-  chomp;
+    my %result = ();
+    @result{@HEADERS} = split($PIPE);
 
-  my %result = ();
-  @result{@HEADERS} = split($PIPE);
+    my $job_submit = Class::Date->new($result{submit});
+    my $job_start  = Class::Date->new($result{start});
+    my $job_end    = Class::Date->new($result{end});
+    my $duration   = ($job_end - $job_start) / 3600;
+    my $waittime   = ($job_start - $job_submit) / 3600;
 
-  my $job_submit = Class::Date->new($result{submit});
-  my $job_start  = Class::Date->new($result{start});
-  my $job_end    = Class::Date->new($result{end});
-  my $duration   = $job_end - $job_start;
-  my $waittime   = $job_start - $job_submit;
+    $csv->add_line({jobid => $result{jobid}, waittime => $waittime, duration => $duration});
+  }
 
-  $csv->add_line({jobid => $result{jobid}, waittime => $waittime, duration => $duration});
+  $start -= $MONTH;
+  $end   -= $MONTH;
+
+  write_file($data_file, $csv->string());
 }
-
-write_file($DATA_FILE, $csv->string());
